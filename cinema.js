31e733e487afd72769322fe406fff43e8e8cf15a
Victorugo238 <victor.trader238@gmail.com>
2022-04-21 18:19:57 -0300

    testando

--- cinema.js
+++ cinema.js
@@ -1,6 +1,15 @@
 var catalogo = require("./catalago.json")
 //console.log(catalogo.data)
 var catalogoString = JSON.stringify(catalogo)
-console.log(catalogoString)
+//console.log(catalogoString)
 var catalogoObjeto = JSON.parse(catalogoString)
-console.log(catalogoObjeto)
+//console.log(catalogoObjeto)
+
+function listarTodosFilmes(filmes){
+    for(let i = 0; i < filmes.length; i++){
+        console.log(filmes[i].titulo);
+
+
+    }
+}
+listarTodosFilmes(catalogoObjeto.data)
