a983bd78739b9e41787e95f4ac3ac6d7a5775dea
Victorugo238 <victor.trader238@gmail.com>
2022-04-26 20:51:12 -0300

    criando exercicio em Cartaz

--- cinema.js
+++ cinema.js
@@ -3,13 +3,27 @@ var catalogo = require("./catalago.json")
 var catalogoString = JSON.stringify(catalogo)
 //console.log(catalogoString)
 var catalogoObjeto = JSON.parse(catalogoString)
-//console.log(catalogoObjeto)
+console.log(catalogoObjeto)
 
 function listarTodosFilmes(filmes){
     for(let i = 0; i < filmes.length; i++){
         console.log(filmes[i].titulo);
 
 
-    }
+   }
 }
 listarTodosFilmes(catalogoObjeto.data)
+
+
+function listarFilmesEmCartaz(filmes) {
+    const filmesEmCartaz = filmes.filter((filme) => {
+      if (filme.emCartaz === true) {
+        return true;
+      }
+    });
+    return filmesEmCartaz;
+  }
+  console.log(listarFilmesEmCartaz(catalogoObj.data));
+  
+
+
